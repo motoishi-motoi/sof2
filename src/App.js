571b1f0a65682dd355ab53ae9939998ab20eae5c
motoishi-motoi <zatsugaku.kaiwa+m@gmail.com>
2021-03-14 16:15:41 +0900

    gtag導入

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import Main from './components/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -5471,12 +5471,24 @@ desc: [
     ],
   }
 
+
+  // gtag処理
+  const router = useRef(null);
+
+  useEffect(() => {
+    router.current.history.listen((location) => {
+      window.gtag('config', 'UA-157580228-1', {
+        'page_path': location.pathname
+      });
+    });
+  });
+
   return (
     <div className = 'main-wrapper'>
       <Header />
       
       
-      <Router>
+      <Router ref={router}>
         <Switch>
           <Route exact path='/' render={ () => <Main mainImage = {intialState.imageList}/>} />
           <Route path='/iceland' render={ () => <IcelandPage mainImage = {intialState.imageList}/>} />
